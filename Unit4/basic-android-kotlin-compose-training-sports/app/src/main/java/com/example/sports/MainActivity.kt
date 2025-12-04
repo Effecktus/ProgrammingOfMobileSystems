1c236bb25bee7a249580706cd192f35bfb6960eb
Effecktus <fqlfh2004@gmail.com>
2025-12-04 22:19:46 +0300

    Completed application Sports

--- Unit4/basic-android-kotlin-compose-training-sports/app/src/main/java/com/example/sports/MainActivity.kt
+++ Unit4/basic-android-kotlin-compose-training-sports/app/src/main/java/com/example/sports/MainActivity.kt
@@ -27,6 +27,8 @@ import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import com.example.sports.ui.SportsApp
@@ -35,6 +37,7 @@ import com.example.sports.ui.theme.SportsTheme
 /**
  * Activity for Sports app
  */
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -51,7 +54,9 @@ class MainActivity : ComponentActivity() {
                                 .calculateEndPadding(layoutDirection)
                         )
                 ) {
+                    val windowSize = calculateWindowSizeClass(this)
                     SportsApp(
+                        windowSize = windowSize.widthSizeClass,
                         onBackPressed = { finish() }
                     )
                 }
